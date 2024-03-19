cab0b294e3234feee6590b8ff6170c12c6b4aae7
Diana Sen <diana_sen@epam.com>
2024-03-18 20:56:50 -0600

    feat: add custom date and duration pipes

--- src/app/shared/pipes/custom-date.pipe.ts
+++ src/app/shared/pipes/custom-date.pipe.ts
@@ -1,8 +1,22 @@
-import { Pipe } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
     name: 'customDate'
 })
-export class CustomDatePipe {
-    // Add your code here
+export class CustomDatePipe implements PipeTransform{
+   
+    transform(value: Date): string {
+        let year = value.getFullYear();
+        let month =this.formatDate(value.getMonth() + 1);
+        let day = this.formatDate(value.getDate());
+        
+        return `${day}.${month}.${year}`
+    }
+
+    formatDate(dateValue: number): string{
+        if(dateValue < 10){
+            return `0${dateValue}`;
+        } 
+        return `${dateValue}`; 
+    }
 }
--- src/app/shared/pipes/duration.pipe.ts
+++ src/app/shared/pipes/duration.pipe.ts
@@ -1,8 +1,19 @@
-import { Pipe } from "@angular/core";
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
     name: 'duration'
 })
-export class DurationPipe {
+export class DurationPipe implements PipeTransform {
     // Add your code here
+    transform(value: number): string {
+        let result = '';
+        const hours: number = Math.floor(value / 60);
+        const minutes: number = Math.round(value % 60);
+        return `${this.formatTime(hours)}:${this.formatTime(minutes)} ${hours <= 1 ? ' hour': ' hours'}`;
+    }
+
+    formatTime(value: number): string {
+        let time = '';
+        return `${value < 10 ? '0' + value : value }`
+    }
 }
