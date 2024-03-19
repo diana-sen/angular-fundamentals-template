d936c0718eb3425e4955919c5f1ef68aa56d4696
Diana Sen <diana_sen@epam.com>
2024-03-19 16:21:14 -0600

    fix: courseForm

--- src/app/shared/components/course-form/course-form.component.ts
+++ src/app/shared/components/course-form/course-form.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   FormBuilder, FormGroup, FormArray, Validators
 } from '@angular/forms';
+import { ButtonConstants } from '@app/app.constants';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 
@@ -10,14 +11,14 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.scss'],
 })
-export class CourseFormComponent implements OnInit{
+export class CourseFormComponent {
+
+  createAuthorText = ButtonConstants.BUTTON_CREATE_AUTHOR;
+  createCourseText = ButtonConstants.BUTTON_CREATE_COURSE;
+  cancelText = ButtonConstants.BUTTON_CANCEL;
+  
   constructor(public fb: FormBuilder, public library: FaIconLibrary) {
     library.addIconPacks(fas);
-  }
-  courseForm!: FormGroup;
-  // Use the names `title`, `description`, `author`, 'authors' (for authors list), `duration` for the form controls.
-
-  ngOnInit(): void {
     this.courseForm = this.fb.group({
       title: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
       description: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
@@ -29,9 +30,8 @@ export class CourseFormComponent implements OnInit{
         name:  Validators.compose([Validators.pattern('^[A-Za-z0-9]+$'), Validators.minLength(2)])
       }),
     });
-
-
-  
   }
+  courseForm!: FormGroup;
+  // Use the names `title`, `description`, `author`, 'authors' (for authors list), `duration` for the form controls.
 
 }
