310d972287b603702167a2028902df1d7357a670
Diana Sen <diana_sen@epam.com>
2024-03-19 17:28:30 -0600

    fix on authors

--- src/app/shared/components/course-form/course-form.component.ts
+++ src/app/shared/components/course-form/course-form.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {
   FormBuilder, FormGroup, FormArray, Validators
 } from '@angular/forms';
-import { ButtonConstants } from '@app/app.constants';
+import { ButtonConstants, IconNames } from '@app/app.constants';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 
@@ -16,18 +16,23 @@ export class CourseFormComponent {
   createAuthorText = ButtonConstants.BUTTON_CREATE_AUTHOR;
   createCourseText = ButtonConstants.BUTTON_CREATE_COURSE;
   cancelText = ButtonConstants.BUTTON_CANCEL;
+  addIcon = IconNames.ADD;
+  deleteICon = IconNames.DELETE;
+
   
   constructor(public fb: FormBuilder, public library: FaIconLibrary) {
     library.addIconPacks(fas);
     this.courseForm = this.fb.group({
       title: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
       description: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      author:['',Validators.minLength(2)],
+      duration: ['',  Validators.compose([Validators.required, Validators.min(0)])],
       authors: this.fb.array([{
         name: [''],
       }]),
-      duration: ['',  Validators.compose([Validators.required, Validators.min(0)])],
+     
       newAuthor: this.fb.group({
-        name:  Validators.compose([Validators.pattern('^[A-Za-z0-9]+$'), Validators.minLength(2)])
+        name:  Validators.compose([Validators.pattern(/^[A-Za-z0-9 ]+$/i), Validators.minLength(2)])
       }),
     });
   }
